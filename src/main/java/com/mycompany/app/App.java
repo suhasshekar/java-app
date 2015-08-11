67d213ebf30b1045ae7e93770e9ba065625ac642
Suhas Shekar <suhasshekar@users.noreply.github.com>
2015-08-11 17:07:35 -0400

    Added Difference function

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,10 @@ public class App
     public int sum(int a , int b){
 		return a+b;
 	}
+
+    public int difference(int a , int b){
+    	return a - b;
+    }	
 	
 		public static void main( String[] args )
     {
